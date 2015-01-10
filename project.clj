882b9c17edb82b81628e069ddf43ac638d653ad2
Taylor Hobbs <taylor@feemon.com>
2015-01-10 10:56:13 -0600

    Refactor: Use environ library

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
 
   :ring {:handler todo.core.handler/app}
 
@@ -12,11 +12,20 @@
                    [org.clojure/java.jdbc "0.3.5"]
                    [postgresql/postgresql "9.1-901-1.jdbc4"]
                    [yesql                 "0.5.0-beta2"]
-                   [midje                 "1.6.3"]]
+                   [environ               "1.0.0"]]
 
   :plugins        [[lein-ring             "0.8.13"]
-                   [lein-midje            "3.1.3"]]
+                   [lein-environ          "1.0.0"]]
 
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :profiles {:test-local {:dependencies [[midje                     "1.6.3"]
+                                         [javax.servlet/servlet-api "2.5"]
+                                         [ring-mock                 "0.1.5"]]
+
+                           :plugins     [[lein-midje                "3.1.3"]]}
+
+             ;; Set these in ./profiles.clj
+             :test-env-vars {}
+             :dev-env-vars  {}
+
+             :test [:test-local :test-env-vars]
+             :dev  [:dev-env-vars]})
