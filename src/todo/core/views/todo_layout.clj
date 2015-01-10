b53e0deda36c5de16883aedac19bbf546b14c024
Taylor Hobbs <taylor@feemon.com>
2015-01-10 16:51:50 -0600

    Enhancement: Added todo mvc css

--- src/todo/core/views/todo_layout.clj
+++ src/todo/core/views/todo_layout.clj
@@ -7,16 +7,12 @@
   (html5
   [:head
    [:title "Todo"]
-   (include-css "/css/todo.css")
-   (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")]
+   (include-css "/css/todo.css")]
   [:body
-   [:div.container
-    [:div.row
-     [:div.col-lg-12
+   [:div#todoapp
+     [:div#header
       [:h1 "Todo"]]
-      [:div.row
-       [:div.col-sm-3
         [:form {:action "/add-todo" :method "post"}
-        [:input.form-control {:type "text" :name "todo" :placeholder "Todo"}]
-         [:button.btn.btn-primary {:type "submit"} "Add Todo"]
-  body]]]]]]))
+        [:input#new-todo {:type "text" :name "todo" :placeholder "Todo"}]
+         [:button#hide-button {:type "submit"} "Add Todo"]
+  body]]]))
