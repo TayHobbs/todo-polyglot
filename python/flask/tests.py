9a490916431878cfe1fc89bbf52bb7419e6a11ef
Taylor Hobbs <taylor@feemon.com>
2015-02-19 21:55:35 -0600

    Python - Flask: Added tests

---
+++ python/flask/tests.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+
+from app.config import basedir
+from app import app, models
+from app.db import db
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_hit_index(self):
+        response = self.app.get('/')
+        self.assertIn('What needs to be done?', response.data)
+
+    def test_todos_show_up_on_index(self):
+        todo = models.Todo(name='first todo')
+        db.session.add(todo)
+        db.session.commit()
+        response = self.app.get('/')
+        self.assertIn('first todo', response.data)
+
+    def test_can_add_todo_through_post_to_add_todo(self):
+        response = self.app.post(
+            '/add-todo',
+            data={'todo': 'New Todo'},
+            follow_redirects=True)
+        self.assertIn('New Todo', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
