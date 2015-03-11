0aaa6c702c26bb34e9879eb6418b7c9dadf15a00
Taylor Hobbs <taylor@feemon.com>
2015-03-11 07:15:30 -0500

    Ruby - Rails: Can delete todos

--- ruby/rails/todos/app/controllers/todos_controller.rb
+++ ruby/rails/todos/app/controllers/todos_controller.rb
@@ -1,29 +1,21 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
   def index
     @todos = Todo.all
     @todo = Todo.new
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos
-  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
 
@@ -38,8 +30,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
@@ -52,8 +42,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
     respond_to do |format|
@@ -63,14 +51,11 @@ class TodosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def todo_params
       params.require(:todo).permit(:name)
-
     end
 end
