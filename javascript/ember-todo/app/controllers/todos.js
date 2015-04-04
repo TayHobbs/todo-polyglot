7dbbb22961e713872789d8babc041fd57cb2e7f4
Taylor Hobbs <taylor@feemon.com>
2015-04-03 20:29:31 -0500

    Javascript - Ember: show number of remaining todos

--- javascript/ember-todo/app/controllers/todos.js
+++ javascript/ember-todo/app/controllers/todos.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  remaining: function() {
+    return this.model.filterBy('completed', false).get('length');
+  }.property('model.@each.completed'),
+
   actions: {
     createTodo: function() {
       var newTodoName = this.get('name');
