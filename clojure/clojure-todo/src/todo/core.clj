6db262e960270bfb88e917f890e3617f9dd200e3
Taylor Hobbs <taylor@feemon.com>
2015-05-28 20:45:11 -0500

    Clojure: Add as many todos as you like

--- clojure/clojure-todo/src/todo/core.clj
+++ clojure/clojure-todo/src/todo/core.clj
@@ -2,14 +2,23 @@
 
 (def next-id (atom 1))
 (def todos (atom {}))
+(defn next-action [])
 
 (defn add-todo []
+  (println "Enter Todo Name")
   (let [todo (read-line)]
     (swap! todos assoc @next-id {:name todo :completed false} ))
-  (swap! next-id inc))
+  (swap! next-id inc)
+  (next-action))
+
+(defn next-action []
+  (println "----------------------------")
+  (println "Current Todos: " (seq @todos))
+  (println "What would you like to do now?")
+  (println "Add new Todo - 'add'")
+  (let [input (.toLowerCase (read-line))]
+    (if (= "add" input)
+      (add-todo))))
 
 (defn -main [& args]
-  (add-todo)
-  (println @todos)
-  (add-todo)
-  (println @todos))
+    (next-action))
